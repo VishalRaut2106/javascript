bdcac91abf347f9e3781d91425b91ca11973f874
Vishal <rautv2005@gmail.com>
2025-04-20 01:14:23 +0530

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,18 @@
+let name = "vishal"
+let age = "19"
+let isLoggedIn = false  
+
+//number =>  2 to power 53
+//bigint 
+// string=>"",''
+// boolean => true/false
+// null => stand alone value
+// undefined => not declared
+// symbol => unique
+
+// object
+console.log(typeof "vishal");       //string  
+console.log(typeof age);            //variable
+console.log(typeof null);           //null is an object
+console.log(typeof undefined);      //undefined
+
