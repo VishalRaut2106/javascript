98c16e3c1174953607f2960677cb3b6890572964
Vishal <rautv2005@gmail.com>
2025-04-20 22:56:36 +0530

    comparison

---
+++ basics/comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log(2 > 1);
+
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+
+
+// === strictly cheack value as well as dataType
+
+console.log("2" === 2);         //false 
