9bb28bbd09094b55cd2e616db4b5db96d8230d3f
Vishal <rautv2005@gmail.com>
2025-04-30 22:28:54 +0530

    object 2

---
+++ 03_objects/objects2.js
@@ -0,0 +1,10 @@
+const course = {
+    coureeName: "chai aur code",
+    price: "3999",
+    CourseInsrtuctor: "vishal"
+}
+// course.CourseInsrtuctor
+
+const{ CourseInsrtuctor} = course
+
+console.log(CourseInsrtuctor);
