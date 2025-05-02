39aa5a9f3632129777fa0774bbfa9d28700dcff7
Vishal <rautv2005@gmail.com>
2025-05-02 08:01:57 +0530

    arrow function

--- BASICS/04_functions/arrowFunction/arrow.js
+++ BASICS/04_functions/arrowFunction/arrow.js
@@ -14,3 +14,42 @@ user.welcomeMessage()
 user.userName = "jon"
 user.welcomeMessage()
 
+
+
+function Chai(){
+    let user = "vishal"
+    console.log(this.user);
+    
+}
+const Chai2 = () => {
+    let user = "vishal"
+    console.log(this);
+    
+}
+
+
+
+
+
+
+Chai()
+
+
+// const addTwo = (num1,num2) => {
+//     return num1+num2
+//     }
+
+
+/*******************
+ * IMPLECET RETURN *
+ *******************/
+/** if you use the parameter within the curly brasec you have to use return  statement 
+ * if you use parantheses you dont need to use return keyword
+ */
+
+// const addTwo = (num1,num2) =>  num1+num2
+const addTwo = (num1,num2) =>  (num1+num2)
+    
+
+    console.log(addTwo(3,4));
+    
