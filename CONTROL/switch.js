11acc2c4015434e9d2fa897d45d71965feb54765
Vishal <rautv2005@gmail.com>
2025-05-03 22:31:30 +0530

    Switch

---
+++ CONTROL/switch.js
@@ -0,0 +1,37 @@
+/**********
+ * SWITCH *
+ **********/
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const Month = 3
+switch (Month) {
+    case 1:
+        console.log("january");
+        
+        break;
+    case 2:
+        console.log("Feb");
+        
+        break;
+    case 3:
+        console.log("mar");
+        
+        break;
+    case 4:
+        console.log("apr");
+        
+        break;
+
+    default:
+    console.log("defult");
+    
+        break;
+}
