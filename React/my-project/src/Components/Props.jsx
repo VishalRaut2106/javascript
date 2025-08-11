f1b74ce9648977aa129d0e59d218d34b5937d401
VishalRaut2106 <rautv2005@gmail.com>
2025-08-11 22:11:38 +0530

    feat: Refactor Props component to Practice_Props with destructured values and enhanced rendering

--- React/my-project/src/Components/Props.jsx
+++ React/my-project/src/Components/Props.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
-function Props() {
-    
+function Practice_Props({values} ) {
+    const {image ,  profession ,name , description} = values;  // Destructuring the values object to get the properties directly. 
+
   return (
-    <div>Props</div>
+    <div className=' rounded overflow-hidden'>
+        <div className="w-60  bg-zinc-100  ">
+            <div className="w-full ">
+                <img className='w-full object-cover h-52 ' src={image} alt="" />
+            </div>
+            <div className="w-full p-3">
+                <h3 className=' text-xl font-semibold'>{name}</h3>
+                <h5 className='text-xx'>{ profession}</h5>
+                <p className='text-xs' >{description}</p>
+                <button className='px-3 mt-4 py-1 text-xs text-shadow-white bg-blue-500 rounded-md'> add Friend</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
-export default Props
+export default Practice_Props
