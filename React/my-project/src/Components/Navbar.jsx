85cdc68fe475bbd1d3eaf423e979840fff65ad2b
VishalRaut2106 <rautv2005@gmail.com>
2025-08-12 23:15:34 +0530

    feat: Update Navbar component to accept data prop and display count of favorites

--- React/my-project/src/Components/Navbar.jsx
+++ React/my-project/src/Components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-function Navbar() {
+function Navbar({data}) {
+
   return (
     <div className="w-full px-20 py-3  flex justify-between items-center">
       <h3>orange</h3>
-      <div className="flex p-4 bg-orange-400 text-white  rounded-md text-xl ">
+      <div className="flex gap-3 p-4 bg-orange-400 text-white  rounded-md text-xl ">
         <h3>Faviourates</h3>
-        <h4>2</h4>
+        <h4>{data.filter(item =>item.added).length}</h4>
       </div>
     </div>
   );
