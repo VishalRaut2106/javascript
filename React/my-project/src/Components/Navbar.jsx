8f2ceb4fc638c518df6bc09fd1df2e237a52e367
VishalRaut2106 <rautv2005@gmail.com>
2025-08-12 23:58:50 +0530

    feat: Update Navbar component styles and improve layout

--- React/my-project/src/Components/Navbar.jsx
+++ React/my-project/src/Components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import styles from "./Style.module.css"
 function Navbar({data}) {
 
   return (
-    <div className="w-full px-20 py-3  flex justify-between items-center">
-      <h3>orange</h3>
+    <div className="w-full px-20 py-3  flex justify-between items-center ">
+      <h3 className={`${styles.a} ${styles.b} p-3 rounded-md font-semibold`}>orange</h3>
       <div className="flex gap-3 p-4 bg-orange-400 text-white  rounded-md text-xl ">
         <h3>Faviourates</h3>
         <h4>{data.filter(item =>item.added).length}</h4>
