d4420ca3cee69bf2f029fb4db609396e4c39dafc
VishalRaut2106 <rautv2005@gmail.com>
2025-08-13 18:38:14 +0530

    feat: Create App2 component and integrate FormHandling

---
+++ React/my-project/src/App2.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import FormHandling from './Components/FormHandling'
+
+function App2() {
+
+
+  return (
+
+
+
+    <>
+    <Formhandling/>
+    
+    </>
+
+  )
+}
+
+export default App2
