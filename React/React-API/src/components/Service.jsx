59dd461fc004418a5d02c24382eb7f14043a6b2b
VishalRaut2106 <rautv2005@gmail.com>
2025-09-13 14:23:53 +0530

    .

--- React/React-API/src/components/Service.jsx
+++ React/React-API/src/components/Service.jsx
@@ -1,16 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Service = () => {
+  const [first, setfirst] = useState("this is first data");
+  const [second, setsecond] = useState("this is imp second data");
 
-    useEffect(()=>{
-        console.log("service is created");
-        return() =>{
-            console.log("service is deleted");
-            
-        }
-    })
+  useEffect(() => {
+    console.log("service is created");
+    return () => {
+      console.log("service is deleted");
+    };
+  });
 
-  return <div>Service</div>;
+  return (
+    <div className="m-10">
+      <h1>{first}</h1>
+      <button
+        onClick={() => setfirst("normal data has been changed")}
+        className="p-3 mb-10  transition-all bg-blue-200 hover:bg-blue-300 rounded-2xl"
+      >
+        Change normal data
+      </button>
+
+      <h1>{second}</h1>
+      <button
+        onClick={() => setsecond("large data has been changed")}
+        className="p-3 mb-10  transition-all bg-blue-200 hover:bg-blue-300 rounded-2xl"
+      >
+        Change large data
+      </button>
+    </div>
+  );
 };
 
 export default Service;
